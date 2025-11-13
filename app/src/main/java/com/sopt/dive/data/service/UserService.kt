ccc63be72e7f90e7815cb228ac1a03e45d41d45b
sssuhha <ysh83@sookmyung.ac.kr>
2025-11-14 03:45:53 +0900

    [#8/feat] UserService 및 UserRepository 추가

---
+++ app/src/main/java/com/sopt/dive/data/service/UserService.kt
@@ -0,0 +1,30 @@
+package com.sopt.dive.data.service
+
+import com.sopt.dive.data.dto.request.RequestLoginDto
+import com.sopt.dive.data.dto.request.RequestSignUpDto
+import com.sopt.dive.data.dto.response.ResponseLoginDto
+import com.sopt.dive.data.dto.response.ResponseSignUpDto
+import com.sopt.dive.data.dto.response.ResponseUserInfoDto
+import com.sopt.dive.data.dto.response.ServerResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface UserService {
+
+    @POST("/api/v1/auth/login")
+    suspend fun login(
+        @Body request: RequestLoginDto
+    ): ResponseLoginDto
+
+    @POST("/api/v1/users")
+    suspend fun signUp(
+        @Body request: RequestSignUpDto
+    ): ServerResponse<ResponseSignUpDto>
+
+    @GET("/api/v1/users/{id}")
+    suspend fun getUserInfo(
+        @Path("id") id: String
+    ): ResponseUserInfoDto
+}
